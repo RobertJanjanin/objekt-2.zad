f5cfc2c9eba348481049ada4edda4e4ae0aba34f
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-03-10 12:16:53 +0100

    Add files via upload

---
+++ statickeMetodeRobertJanjanin/statickeMetodeRobertJanjanin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeMetodeRobertJanjanin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa prvi = new PrvaKlasa("Pero", 2003);
+            Console.WriteLine(prvi.ToString());
+            PrvaKlasa drugi = new PrvaKlasa("Ivica", 2004);
+            Console.WriteLine(drugi.ToString());
+            PrvaKlasa treci = new PrvaKlasa("Ibro", 2001);
+            Console.WriteLine(treci.ToString());
+            treci.Ime = "Milorad";
+            Console.WriteLine(treci.Ime);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ statickeMetodeRobertJanjanin/statickeMetodeRobertJanjanin/PrvaKlasa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeMetodeRobertJanjanin
+{
+    class PrvaKlasa
+    {
+        string ime;
+        int godina;
+        static int brojac;
+        public string Ime { get => ime; set => ime = value; }
+        public int Godina { get => godina; set => godina = value; }
+
+        public PrvaKlasa(string ime, int godina)
+        {
+            brojac++;
+            this.ime = ime;
+            this.godina = godina;
+        }
+        public override string ToString()
+        {
+           string ispis = "\nIme: " + ime + "\nGodiste: " + godina + "\nRedni broj: " + brojac;
+            return ispis;
+        }
+    }
+}
